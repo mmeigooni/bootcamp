ba0188e27bbaca92df4da72caf0cbbe712f54ef4
Muhammad Meigooni <muhammad@themakersquare.com>
2015-02-16 23:54:20 -0800

    added fade-in bootstrap navbar

---
+++ public/js/scripts.js
@@ -0,0 +1,22 @@
+(function ($) {
+  $(document).ready(function(){
+    
+  // hide .navbar first
+  $(".navbar2").hide();
+  
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+            // set distance user needs to scroll before we fadeIn navbar
+      if ($(this).scrollTop() > 550) {
+        $('.navbar2').fadeIn();
+      } else {
+        $('.navbar2').fadeOut();
+      }
+    });
+
+  
+  });
+
+});
+  }(jQuery));
