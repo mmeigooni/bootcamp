d6f3dbb92cb249ff8236ce7e8442dc8715330bf8
Muhammad Meigooni <muhammad@themakersquare.com>
2015-02-17 12:18:08 -0800

    double functioning nabbers

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,16 +2,16 @@
   $(document).ready(function(){
     
   // hide .navbar first
-  $(".navbar2").hide();
+  $("#navbar2").hide();
   
   // fade in .navbar
   $(function () {
     $(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
       if ($(this).scrollTop() > 550) {
-        $('.navbar2').fadeIn();
+        $('#navbar2').fadeIn();
       } else {
-        $('.navbar2').fadeOut();
+        $('#navbar2').fadeOut();
       }
     });
 
