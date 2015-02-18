70fd0e2fcf04aee58a7c728eb8e23b194b5accfc
Muhammad Meigooni <muhammad@themakersquare.com>
2015-02-17 18:53:33 -0800

    added ajax form and viewport meta tag

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 (function ($) {
   $(document).ready(function(){
-    
+
   // hide .navbar first
   $("#navbar2").hide();
   
@@ -8,15 +8,42 @@
   $(function () {
     $(window).scroll(function () {
             // set distance user needs to scroll before we fadeIn navbar
-      if ($(this).scrollTop() > 550) {
-        $('#navbar2').fadeIn();
-      } else {
-        $('#navbar2').fadeOut();
-      }
-    });
+            if ($(this).scrollTop() > 550) {
+              $('#navbar2').fadeIn();
+            } else {
+              $('#navbar2').fadeOut();
+            }
+          });
 
-  
+
   });
 
 });
-  }(jQuery));
+}(jQuery));
+
+$(document).ready(function(){
+  $("#submit").click(function(){
+    var email = $("#subscribe-email").val();
+
+    var dataString = 'email='+ email;
+    if(email=='')
+    {
+      alert("Please Fill All Fields");
+    }
+    else
+    {
+// AJAX Code To Submit Form.
+$.ajax({
+
+  url: "//formspree.io/admin@mushroomlearning.com", 
+  type: "POST",
+  data: dataString,
+  cache: false,
+  success: function(result){
+    alert(result);
+  }
+});
+}
+return false;
+});
+});
